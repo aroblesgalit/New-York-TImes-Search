5e77012154ab5bc658c070f36fd7008450cd2ab9
Alvin Galit <aroblesgalit@gmail.com>
2020-03-27 12:00:09 -0500

    Updated id's on html file

---
+++ script.js
@@ -0,0 +1,13 @@
+// Listen to a click event on the search button
+$("#searchBtn").on("click", function(e) {
+  e.preventDefault();
+
+  // Get data from the form and store into variables
+  
+})
+
+
+
+
+
+// Listen to a click event on the clear button
